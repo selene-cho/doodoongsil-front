fe3e4f67338b3b60f709cf839e2813cd98c9f2f9
selene-cho <selene.cho29@gmail.com>
2023-12-08 15:35:50 +0900

    fix: Button 컴포넌트 수정

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-export default function Button({ children, onClick, className }) {
+export default function Button({
+  children,
+  type = 'button',
+  onClick,
+  className,
+}) {
   return (
     <button
+      type={type}
       onClick={onClick}
-      className={`${className} flex w-fit items-center justify-center rounded`}
+      className={`flex w-fit items-center justify-center rounded px-2 py-1 ${className} `}
     >
       {children}
     </button>
