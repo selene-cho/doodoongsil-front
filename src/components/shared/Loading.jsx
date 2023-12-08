1a351904a929e5bdc4cb3469d7ab3b123731904f
selene-cho <selene.cho29@gmail.com>
2023-12-08 15:35:24 +0900

    fix: Loading 컴포넌트 className props 추가

--- src/components/shared/Loading.jsx
+++ src/components/shared/Loading.jsx
@@ -1,9 +1,15 @@
-export default function Loading() {
+export default function Loading({ className }) {
   return (
     <div className="flex gap-2">
-      <div className="h-1 w-1 animate-bounce rounded-full bg-blue-600 [animation-delay:.7s]"></div>
-      <div className="h-1 w-1 animate-bounce rounded-full bg-blue-600 [animation-delay:.3s]"></div>
-      <div className="h-1 w-1 animate-bounce rounded-full bg-blue-600 [animation-delay:.7s]"></div>
+      <div
+        className={`h-1 w-1 animate-bounce rounded-full bg-blue-600 [animation-delay:.7s] ${className}`}
+      ></div>
+      <div
+        className={`h-1 w-1 animate-bounce rounded-full bg-blue-600 [animation-delay:.3s] ${className}`}
+      ></div>
+      <div
+        className={`h-1 w-1 animate-bounce rounded-full bg-blue-600 [animation-delay:.7s] ${className}`}
+      ></div>
     </div>
   );
 }
