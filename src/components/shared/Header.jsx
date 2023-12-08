ad566eb38d903cfff97cd0263abcefc91b34c1bf
selene-cho <selene.cho29@gmail.com>
2023-12-08 15:36:34 +0900

    feat: Header 컴포넌트

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,8 +1,20 @@
-export default function Header({ children, className }) {
+import { useNavigate } from 'react-router-dom';
+import { TbArrowNarrowLeft } from 'react-icons/tb';
+
+import Button from './Button';
+
+export default function Header({ children, className, backLink }) {
+  const navigate = useNavigate();
+
   return (
     <header
-      className={`${className} mb-2 border-b border-slate-300 p-3 text-center font-bold`}
+      className={`relative mb-5 flex h-[90px] w-full items-center justify-center border-b border-slate-300 pb-5 pt-10 text-center text-lg font-extrabold ${className}`}
     >
+      {backLink && (
+        <Button onClick={() => navigate(-1)}>
+          <TbArrowNarrowLeft className="absolute left-5 text-2xl" />
+        </Button>
+      )}
       {children}
     </header>
   );
