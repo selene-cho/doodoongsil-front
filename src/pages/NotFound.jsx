db6119b3df35636a080bafbe07fb95a8f04f1cb1
selene-cho <selene.cho29@gmail.com>
2023-12-08 15:36:12 +0900

    feat: Notfound 컴포넌트

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,19 @@
+import { TbAlertTriangleFilled } from 'react-icons/tb';
+
+import Header from '../components/shared/Header';
+
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="h-screen flex-col items-center justify-center">
+      <Header backLink />
+      <div className="my-auto flex h-[calc(100vh-110px)] flex-col items-center justify-center">
+        <TbAlertTriangleFilled className="mb-3 text-3xl text-amber-500" />
+        <h1 className="mb-8 text-xl font-extrabold">
+          페이지를 찾을 수 없습니다
+        </h1>
+        <p>요청을 처리하는 중 오류가 발생했습니다</p>
+        <p>잠시후 다시 시도해주세요</p>
+      </div>
+    </div>
+  );
 }
